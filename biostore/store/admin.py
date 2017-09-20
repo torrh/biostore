1251ec31ed9e0b8db1332a54e6a152d5c61f38e7
Hawer Torres <ha.torres11@uniandes.edu.co>
2017-09-20 00:03:58 -0500

    Create models related to a product and its relations.

--- biostore/store/admin.py
+++ biostore/store/admin.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Product
+from .models import ProductType
+from .models import Category
 
 # Register your models here.
+
+admin.site.register(Product)
+admin.site.register(ProductType)
+admin.site.register(Category)
--- biostore/store/models.py
+++ biostore/store/models.py
@@ -4,3 +4,21 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class ProductType(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    url = models.CharField(max_length=1000)
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.CharField(max_length=1000)
+
+class Product(models.Model):
+    create_at = models.DateTimeField(auto_now_add=True)
+    unit_price = models.FloatField()
+    count = models.IntegerField()
+    unit_type = models.CharField(max_length=255)
+    available_at = models.DateField()
+    productType = models.ForeignKey(ProductType)
+    category = models.ForeignKey(Category)
