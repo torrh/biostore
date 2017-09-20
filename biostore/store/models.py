9aa780ed5a2796187e4ce663f64461ba949158c9
Hawer Torres <ha.torres11@uniandes.edu.co>
2017-09-20 14:54:44 -0500

    Add new Administrator, Producer, Cooperative, CommonOfert models

--- biostore/store/models.py
+++ biostore/store/models.py
@@ -22,3 +22,49 @@ class Product(models.Model):
     available_at = models.DateField()
     productType = models.ForeignKey(ProductType)
     category = models.ForeignKey(Category)
+
+class Administrator(models.Model):
+    name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+
+class Producer(models.Model):
+    uid = models.FloatField()
+    email = models.EmailField()
+    name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    phone_number = models.CharField(max_length=255)
+
+class Cooperative(models.Model):
+    administrator = models.ForeignKey(Administrator)
+
+class CommonOffer(models.Model):
+    create_at = models.DateTimeField(auto_now_add=True)
+    delivery_at = models.DateField()
+    products = models.ManyToManyField(Product)
+
+class ProducerOffer(CommonOffer):
+    producer = models.ForeignKey(Producer)
+
+class CooperativeOffer(CommonOffer):
+    cooperative = models.ForeignKey(Cooperative)
+
+class Consumer(models.Model):
+    uid = models.FloatField()
+    email = models.EmailField()
+    name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    phone_number = models.CharField(max_length=255)
+
+class Order(models.Model):
+    create_at = models.DateTimeField(auto_now_add=True)
+    delivery_at = models.DateField()
+    shipping_address = models.CharField(max_length=255)
+    consumer = models.ForeignKey(Consumer)
+    products = models.ManyToManyField(Product)
