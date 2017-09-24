82e842ad2d90ea8c55831ab05e3f5fecb107cf80
cristianABC <cristianarangodaza@gmail.com>
2017-09-24 00:18:12 -0500

    REST css framework

--- biostore/settings.py
+++ biostore/settings.py
@@ -123,7 +123,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_ROOT = '/home/django/django_project/django_project/static'
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # REST Framework
 
