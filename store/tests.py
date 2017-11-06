aedbf40972b912f97d16494c82839c961ad19bd7
Cristian Arango Daza <cl.arango741@uniandes.edu.co>
2017-11-05 20:03:21 -0500

    RED: registrar productor

--- store/tests.py
+++ store/tests.py
@@ -4,3 +4,16 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Producer
+
+
+class  ProducerTestCase(TestCase):
+    def setUp(self):
+     uid = Producer.objects.latest('uid').uid + 1
+     Producer.objects.create(uid=uid, name="test", last_name="test", email="test@test.com", address="test address",
+                                               password="test",
+                                               phone_number="1234",latitude="0",longitude="0", url="url",farmurl="farmurl" )
+     def test_addedproducer(self):
+        producer = Producer.objects.get(name="test")
+        self.assertEqual(producer.last_name, "t")
+
