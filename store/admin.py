d00657f91d57e6a1065229ab897f87b2b7aa95f9
Cristian Arango Daza <cristianarangodaza@gmail.com>
2017-11-29 14:30:07 -0500

    Admin Configuration Issues

--- store/admin.py
+++ store/admin.py
@@ -21,13 +21,25 @@ class ProducerDisplay(admin.ModelAdmin):
 class NotificationDisplay(admin.ModelAdmin):
     list_display = ('title', 'text', 'img')
     search_fields = ['title']
+
+class OrderDisplay(admin.ModelAdmin):
+    list_display = ('consumer', 'shipping_address', 'delivery_at')
+    search_fields = ['consumer']
+class ConsumerDisplay(admin.ModelAdmin):
+    list_display = ('name', 'email', 'last_name')
+    search_fields = ['name']
+
+class OrderItemDisplay(admin.ModelAdmin):
+    list_display = ('count', 'offer', 'state')
+    search_fields = ['state']
+
 admin.site.register(ProductType)
 admin.site.register(Category)
 admin.site.register(PaymentType)
 admin.site.register(Payment)
-admin.site.register(Order)
-admin.site.register(Consumer)
-admin.site.register(Order_Item)
+admin.site.register(Order, OrderDisplay)
+admin.site.register(Consumer, ConsumerDisplay)
+admin.site.register(Order_Item, OrderItemDisplay)
 admin.site.register(Producer, ProducerDisplay)
 admin.site.register(ProducerOffer)
 admin.site.register(AdminOffer)
